a039b8a1c30e43dc27b8baa82f0bbf7b271bd20c
John Gietzen <john@gietzen.us>
2018-04-14 15:07:43 -0700

    Created the RandomAccessPerlinNoise project.

---
+++ RandomAccessPerlinNoise.Tests/Class1Tests.cs
@@ -0,0 +1,15 @@
+// Copyright © John Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace RandomAccessPerlinNoise.Tests
+{
+    using Xunit;
+
+    public class Class1Tests
+    {
+        [Fact]
+        public void Who_When_What()
+        {
+            var subject = new Class1();
+        }
+    }
+}
